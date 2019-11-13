29412144b6022171daf9ba7fe72b2bedbb82db68
Pedro Teixeira <pcdateixeira@gmail.com>
2019-11-13 11:11:30 -0300

    Hello world

---
+++ obj/Debug/90/android/typemap.jm.arm64-v8a.s
@@ -0,0 +1,26 @@
+	/* Data SHA1: 0d65eac23fb75ca8ed2938bb3e98edcdc6506463 */
+	.arch	armv8-a
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.word	1
+	/* entry-count */
+	.word	1034
+	/* entry-length */
+	.word	262
+	/* value-offset */
+	.word	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 270909
+	.include	"typemap.jm.inc"
